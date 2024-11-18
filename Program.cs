c8d894606a965c8d016ef9f85ebf86d8089d6d21
Vulnorn <mixadrbk31@gmail.com>
2024-11-18 14:38:02 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        bool isEnterNumber = true;
+
+        while (isEnterNumber)
+        {
+            Console.WriteLine($"Введите число.");
+
+            if (Utilite.TryGetNumber(out int number) == true)
+            {
+                Console.WriteLine($"Число - {number}");
+                isEnterNumber = false;
+            }          
+        }
+    }
+
+    class Utilite
+    {
+        public static bool TryGetNumber(out int numder)
+        {
+            string userInput;
+
+            do
+            {
+                userInput = Console.ReadLine();
+            }
+            while (TryGetInputValue(userInput, out numder));
+            return true;
+        }
+
+        private static bool TryGetInputValue(string input, out int number)
+        {
+            if (int.TryParse(input, out number) == false)
+            {
+                Console.WriteLine("Не корректный ввод.");
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
