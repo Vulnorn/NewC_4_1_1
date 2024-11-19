507e9292ec889db63da5f3012220e473a86d7598
Vulnorn <mixadrbk31@gmail.com>
2024-11-19 10:56:45 +0300

    2

--- Program.cs
+++ Program.cs
@@ -13,47 +13,25 @@
             Console.WriteLine($"Число - {number}");
         }
 
-        public static int GetNumber()
+        private static int GetNumber()
         {
             bool isEnterNumber = true;
             int enterNumber=0;
+            string userInput;
 
             while (isEnterNumber)
             {
                 Console.WriteLine($"Введите число.");
 
-                if (TryGetNumber(out int number) == true)
-                {
+                userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out enterNumber) == false)
+                    Console.WriteLine("Не корректный ввод.");
+                else
                     isEnterNumber = false;
-                    enterNumber = number;
-                }
             }
 
             return enterNumber;
-        }
-
-        public static bool TryGetNumber(out int numder)
-        {
-            string userInput;
-
-            do
-            {
-                userInput = Console.ReadLine();
-            }
-            while (TryGetInputValue(userInput, out numder));
-
-            return true;
-        }
-
-        private static bool TryGetInputValue(string input, out int number)
-        {
-            if (int.TryParse(input, out number) == false)
-            {
-                Console.WriteLine("Не корректный ввод.");
-                return true;
-            }
-
-            return false;
-        }
+        }    
     }
 }
